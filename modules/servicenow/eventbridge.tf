0a6d89be0b66e749d56d1a4d7ca15c76326bb198
Marcel de Vroed <mdevroed@schubergphilis.com>
2024-06-24 11:14:20 +0200

    make it complete

--- modules/servicenow/eventbridge.tf
+++ modules/servicenow/eventbridge.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_event_rule" "securityhub" {
   name        = "snow-RuleLifeCycleEvents"
   description = "Send Security Hub imported findings to the AwsServiceManagementConnectorForSecurityHubQueue SQS."
   event_pattern = templatefile("${path.module}/templates/findings_filter.json.tftpl", {
-  severity_filter = jsonencode(var.severity_label_filter) })
+  severity_label_filter = jsonencode(var.severity_label_filter) })
 }
 
 resource "aws_cloudwatch_event_target" "securityhub" {
