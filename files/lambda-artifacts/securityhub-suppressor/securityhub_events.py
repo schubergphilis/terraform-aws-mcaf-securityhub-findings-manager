8e81a6b73f97aaf6055e537499ac4143473a3a59
Carlo van Overbeek <cvanoverbeek@schubergphilis.com>
2024-07-29 16:24:20 -0400

    feat gsn-11066: use latest version of awsfindingsmanagerlib

--- files/lambda-artifacts/securityhub-suppressor/securityhub_events.py
+++ files/lambda-artifacts/securityhub-suppressor/securityhub_events.py
@@ -1,38 +1,22 @@
 from os import environ
 
-import boto3
-import yaml
 from aws_lambda_powertools import Logger
-from awsfindingsmanagerlib.awsfindingsmanagerlib import FindingsManager
-from awsfindingsmanagerlib.backends import Backend
+from awsfindingsmanagerlib import FindingsManager, S3
 
 LOGGER = Logger()
 S3_BUCKET_NAME = environ.get("S3_BUCKET_NAME")
 S3_OBJECT_NAME = environ.get("S3_OBJECT_NAME")
 
-
-class S3(Backend):
-    def __init__(self, bucket_name, file_name):
-        self._file_contents = self._get_file_contents(bucket_name, file_name)
-
-    @staticmethod
-    def _get_file_contents(bucket_name, file_name):
-        s3 = boto3.resource("s3")
-        return s3.Object(bucket_name, file_name).get()["Body"].read()
-
-    def _get_rules(self):
-        data = yaml.safe_load(self._file_contents)
-        return data.get("Rules")
+_s3_backend = S3(S3_BUCKET_NAME, S3_OBJECT_NAME)
+RULES = _s3_backend.get_rules()
+FINDINGS_MANAGER = FindingsManager()
+FINDINGS_MANAGER.register_rules(RULES)
 
 
 @LOGGER.inject_lambda_context(log_event=True)
 def lambda_handler(event, context):
-    s3_backend = S3(S3_BUCKET_NAME, S3_OBJECT_NAME)
-    rules = s3_backend.get_rules()
-    LOGGER.info(rules)
-    findings_manager = FindingsManager()
-    findings_manager.register_rules(rules)
-    if findings_manager.suppress_matching_findings():
+    LOGGER.info(RULES)
+    if FINDINGS_MANAGER.suppress_findings_on_matching_rules(event["detail"]["findings"]):
         LOGGER.info("Successfully applied all suppression rules.")
         return {"finding_state": "suppressed"}
     else:
--- files/lambda-artifacts/securityhub-suppressor/securityhub_trigger.py
+++ files/lambda-artifacts/securityhub-suppressor/securityhub_trigger.py
@@ -1,28 +1,11 @@
 from os import environ
 
-import boto3
-import yaml
 from aws_lambda_powertools import Logger
-from awsfindingsmanagerlib.awsfindingsmanagerlib import FindingsManager
-from awsfindingsmanagerlib.backends import Backend
+from awsfindingsmanagerlib import FindingsManager, S3
 
 LOGGER = Logger()
 S3_BUCKET_NAME = environ.get("S3_BUCKET_NAME")
 S3_OBJECT_NAME = environ.get("S3_OBJECT_NAME")
-
-
-class S3(Backend):
-    def __init__(self, bucket_name, file_name):
-        self._file_contents = self._get_file_contents(bucket_name, file_name)
-
-    @staticmethod
-    def _get_file_contents(bucket_name, file_name):
-        s3 = boto3.resource("s3")
-        return s3.Object(bucket_name, file_name).get()["Body"].read()
-
-    def _get_rules(self):
-        data = yaml.safe_load(self._file_contents)
-        return data.get("Rules")
 
 
 @LOGGER.inject_lambda_context(log_event=True)
