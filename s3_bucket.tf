459ae48b35f0d6049762a61dc314975e014f27b8
Marwin Baumann <56264798+marwinbaumannsbp@users.noreply.github.com>
2024-09-12 14:55:43 +0200

    feature: upgrade python version from 3.8 to 3.12 & package lambda code using github action (#42)
    feature: upgrade python version from 3.8 to 3.12 & package lambda code using github action (#42)
    ---------
    Co-authored-by: marwinbaumannsbp <marwinbaumannsbp@users.noreply.github.com>
    Co-authored-by: Joeri-Abbo <Joeri-Abbo@users.noreply.github.com>

---
+++ s3_bucket.tf
@@ -0,0 +1,32 @@
+# S3 bucket to store Lambda artifacts and the rules list
+module "findings_manager_bucket" {
+  #checkov:skip=CKV_AWS_145:Bug in CheckOV https://github.com/bridgecrewio/checkov/issues/3847
+  #checkov:skip=CKV_AWS_19:Bug in CheckOV https://github.com/bridgecrewio/checkov/issues/3847
+  source  = "schubergphilis/mcaf-s3/aws"
+  version = "~> 0.14.1"
+
+  name        = var.s3_bucket_name
+  kms_key_arn = var.kms_key_arn
+  logging     = null
+  tags        = var.tags
+  versioning  = true
+
+  lifecycle_rule = [
+    {
+      id      = "default"
+      enabled = true
+
+      abort_incomplete_multipart_upload = {
+        days_after_initiation = 7
+      }
+
+      expiration = {
+        expired_object_delete_marker = true
+      }
+
+      noncurrent_version_expiration = {
+        noncurrent_days = 7
+      }
+    }
+  ]
+}
--- versions.tf
+++ versions.tf
@@ -2,9 +2,17 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 4.9"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 2.0"
     }
     local = {
       source  = "hashicorp/local"
