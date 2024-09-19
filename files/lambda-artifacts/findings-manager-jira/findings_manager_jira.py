5fe8d14fb8750fa320a4ab1d6ceb7e26c15c2301
Martin Lefler <mlefler@schubergphilis.com>
2024-09-19 12:47:00 +0200

    python linting

--- files/lambda-artifacts/findings-manager-jira/findings_manager_jira.py
+++ files/lambda-artifacts/findings-manager-jira/findings_manager_jira.py
@@ -1,6 +1,7 @@
-import boto3
 import json
 import os
+
+import boto3
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
@@ -17,51 +18,60 @@ REQUIRED_ENV_VARS = [
 DEFAULT_JIRA_AUTOCLOSE_COMMENT = 'Security Hub finding has been resolved. Autoclosing the issue.'
 DEFAULT_JIRA_AUTOCLOSE_TRANSITION = 'Done'
 
+
 @logger.inject_lambda_context
 def lambda_handler(event: dict, context: LambdaContext):
-  # Validate required environment variables
-  helpers.validate_env_vars(REQUIRED_ENV_VARS)
+    # Validate required environment variables
+    helpers.validate_env_vars(REQUIRED_ENV_VARS)
 
-  # Retrieve environment variables
-  EXCLUDE_ACCOUNT_FILTER = os.environ['EXCLUDE_ACCOUNT_FILTER']
-  JIRA_AUTOCLOSE_ENABLED = os.getenv('JIRA_AUTOCLOSE_ENABLED', DEFAULT_JIRA_AUTOCLOSE_ENABLED)
-  JIRA_AUTOCLOSE_COMMENT = os.getenv('JIRA_AUTOCLOSE_COMMENT', DEFAULT_JIRA_AUTOCLOSE_COMMENT)
-  JIRA_AUTOCLOSE_TRANSITION = os.getenv('JIRA_AUTOCLOSE_TRANSITION', DEFAULT_JIRA_AUTOCLOSE_TRANSITION)
-  JIRA_ISSUE_TYPE = os.environ['JIRA_ISSUE_TYPE']
-  JIRA_PROJECT_KEY = os.environ['JIRA_PROJECT_KEY']
-  JIRA_SECRET_ARN = os.environ['JIRA_SECRET_ARN']
+    # Retrieve environment variables
+    exclude_account_filter = os.environ['EXCLUDE_ACCOUNT_FILTER']
+    jira_autoclose_comment = os.getenv(
+        'JIRA_AUTOCLOSE_COMMENT', DEFAULT_JIRA_AUTOCLOSE_COMMENT)
+    jira_autoclose_transition = os.getenv(
+        'JIRA_AUTOCLOSE_TRANSITION', DEFAULT_JIRA_AUTOCLOSE_TRANSITION)
+    jira_issue_type = os.environ['JIRA_ISSUE_TYPE']
+    jira_project_key = os.environ['JIRA_PROJECT_KEY']
+    jira_secret_arn = os.environ['JIRA_SECRET_ARN']
 
-  # Retrieve JIRA client
-  jira_secret = helpers.get_secret(secretsmanager, JIRA_SECRET_ARN)
-  jira_client = helpers.get_jira_client(jira_secret)
+    # Retrieve JIRA client
+    jira_secret = helpers.get_secret(secretsmanager, jira_secret_arn)
+    jira_client = helpers.get_jira_client(jira_secret)
 
-  # Get Sechub event details
-  event_detail = event['detail']
-  finding = event_detail['findings'][0]
-  finding_account_id = finding['AwsAccountId']
-  workflow_status = finding['Workflow']['Status']
+    # Get Sechub event details
+    event_detail = event['detail']
+    finding = event_detail['findings'][0]
+    finding_account_id = finding['AwsAccountId']
+    workflow_status = finding['Workflow']['Status']
 
-  # Only process finding if account is not excluded
-  if finding_account_id in EXCLUDE_ACCOUNT_FILTER:
-    logger.info(f"Account {finding_account_id} is excluded from JIRA ticket creation.")
-    return
+    # Only process finding if account is not excluded
+    if finding_account_id in exclude_account_filter:
+        logger.info(
+            f"Account {finding_account_id} is excluded from JIRA ticket creation.")
+        return
 
-  if workflow_status == "NEW":
-    # Create JIRA issue and updates Security Hub status to NOTIFIED and adds JIRA issue key to note (in JSON format)
-    issue = helpers.create_jira_issue(jira_client, JIRA_PROJECT_KEY, JIRA_ISSUE_TYPE, event_detail)
-    note = json.dumps({'jiraIssue': issue.key})
-    helpers.update_security_hub(securityhub, finding["Id"], finding["ProductArn"], "NOTIFIED", note)
-  elif workflow_status == "RESOLVED":
-    # Close JIRA issue if finding is resolved. Note text should contain JIRA issue key in JSON format
-    try:
-      note_text = finding['Note']['Text']
-      note_text_json = json.loads(note_text)
-      jira_issue_id = note_text_json.get('jiraIssue')
+    if workflow_status == "NEW":
+        # Create JIRA issue and updates Security Hub status to NOTIFIED
+        # and adds JIRA issue key to note (in JSON format)
+        issue = helpers.create_jira_issue(
+            jira_client, jira_project_key, jira_issue_type, event_detail)
+        note = json.dumps({'jiraIssue': issue.key})
+        helpers.update_security_hub(
+            securityhub, finding["Id"], finding["ProductArn"], "NOTIFIED", note)
+    elif workflow_status == "RESOLVED":
+        # Close JIRA issue if finding is resolved.
+        # Note text should contain JIRA issue key in JSON format
+        try:
+            note_text = finding['Note']['Text']
+            note_text_json = json.loads(note_text)
+            jira_issue_id = note_text_json.get('jiraIssue')
 
-      if jira_issue_id:
-        issue = jira_client.issue(jira_issue_id)
-        helpers.close_jira_issue(jira_client, issue, JIRA_AUTOCLOSE_TRANSITION, JIRA_AUTOCLOSE_COMMENT)
-    except json.JSONDecodeError as e:
-      logger.error(f"Failed to decode JSON from note text: {e}. Cannot autoclose.")
-    except Exception as e:
-      logger.error(f"Failed to get JIRA issue: {e}. Cannot autoclose.")
+            if jira_issue_id:
+                issue = jira_client.issue(jira_issue_id)
+                helpers.close_jira_issue(
+                    jira_client, issue, jira_autoclose_transition, jira_autoclose_comment)
+        except json.JSONDecodeError as e:
+            logger.error(f"Failed to decode JSON from note text: {
+                         e}. Cannot autoclose.")
+        except Exception as e:
+            logger.error(f"Failed to get JIRA issue: {e}. Cannot autoclose.")
--- files/lambda-artifacts/findings-manager-jira/helpers.py
+++ files/lambda-artifacts/findings-manager-jira/helpers.py
@@ -1,209 +1,224 @@
 import base64
 import json
 import os
+from typing import List, Dict
+
 from aws_lambda_powertools import Logger
 from botocore.client import BaseClient
 from botocore.exceptions import ClientError
 from jira import JIRA
 from jira.resources import Issue
-from typing import List, Dict
 
 logger = Logger()
 
+
 def validate_env_vars(env_vars: List[str]) -> None:
-  """
-  Validate that all specified environment variables are set.
+    """
+    Validate that all specified environment variables are set.
 
-  Args:
-      env_vars (List[str]): A list of environment variable names to check.
+    Args:
+        env_vars (List[str]): A list of environment variable names to check.
 
-  Raises:
-      ValueError: If any of the specified environment variables are not set.
-  """
+    Raises:
+        ValueError: If any of the specified environment variables are not set.
+    """
 
-  missing_vars = [var for var in env_vars if var not in os.environ]
+    missing_vars = [var for var in env_vars if var not in os.environ]
 
-  for var in missing_vars:
-    logger.error(f"Environment variable {var} is not set!")
+    for var in missing_vars:
+        logger.error(f"Environment variable {var} is not set!")
 
-  if missing_vars:
-    raise ValueError(f"Missing environment variables: {', '.join(missing_vars)}")
+    if missing_vars:
+        raise ValueError(f"Missing environment variables: {
+                         ', '.join(missing_vars)}")
 
 
 def get_jira_client(jira_secret: Dict[str, str]) -> JIRA:
-  """
-  Create a JIRA client instance using the specified secret.
+    """
+    Create a JIRA client instance using the specified secret.
 
-  Args:
-      jira_secret (Dict[str, str]): A dictionary containing the JIRA connection details.
+    Args:
+        jira_secret (Dict[str, str]): A dictionary containing the JIRA connection details.
 
-  Returns:
-      JIRA: A JIRA client instance.
+    Returns:
+        JIRA: A JIRA client instance.
 
-  Raises:
-      ValueError: If the JIRA connection details are not valid.
-  """
+    Raises:
+        ValueError: If the JIRA connection details are not valid.
+    """
 
-  jira_url = jira_secret.get('url')
-  jira_user = jira_secret.get('apiuser')
-  jira_password = jira_secret.get('apikey')
+    jira_url = jira_secret.get('url')
+    jira_user = jira_secret.get('apiuser')
+    jira_password = jira_secret.get('apikey')
 
-  if not jira_url or not jira_user or not jira_password:
-    raise ValueError("JIRA connection details are not valid!")
+    if not jira_url or not jira_user or not jira_password:
+        raise ValueError("JIRA connection details are not valid!")
 
-  return JIRA(server=jira_url, basic_auth=(jira_user, jira_password))
+    return JIRA(server=jira_url, basic_auth=(jira_user, jira_password))
 
 
 def get_secret(client: BaseClient, secret_arn: str) -> Dict[str, str]:
-  """
-  Retrieve a secret from AWS Secrets Manager.
+    """
+    Retrieve a secret from AWS Secrets Manager.
 
-  Args:
-      client (BaseClient): A boto3 client instance for Secrets Manager.
-      secret_arn (str): The ARN of the secret to retrieve.
+    Args:
+        client (BaseClient): A boto3 client instance for Secrets Manager.
+        secret_arn (str): The ARN of the secret to retrieve.
 
-  Returns:
-      Dict[str, str]: The secret value as a dictionary.
+    Returns:
+        Dict[str, str]: The secret value as a dictionary.
 
-  Raises:
-      ValueError: If the client is not an instance of Secrets Manager.
-      ClientError: If there is an error retrieving the secret.
-  """
+    Raises:
+        ValueError: If the client is not an instance of Secrets Manager.
+        ClientError: If there is an error retrieving the secret.
+    """
 
-  # Validate that the client is an instance of botocore.client.SecretsManager
-  if client.meta.service_model.service_name != 'secretsmanager':
-    raise ValueError(f"Client must be an instance of botocore.client.SecretsManager. Got {type(client)} instead.")
+    # Validate that the client is an instance of botocore.client.SecretsManager
+    if client.meta.service_model.service_name != 'secretsmanager':
+        raise ValueError(f"Client must be an instance of botocore.client.SecretsManager. Got {
+                         type(client)} instead.")
 
-  try:
-    response = client.get_secret_value(SecretId=secret_arn)
-  except ClientError as e:
-    logger.error(f"Error retrieving secret: {e}")
-    raise e
+    try:
+        response = client.get_secret_value(SecretId=secret_arn)
+    except ClientError as e:
+        logger.error(f"Error retrieving secret: {e}")
+        raise e
 
-  secret = response.get('SecretString')
-  if secret is None:
-    secret = base64.b64decode(response['SecretBinary']).decode('utf-8')
+    secret = response.get('SecretString')
+    if secret is None:
+        secret = base64.b64decode(response['SecretBinary']).decode('utf-8')
 
-  return json.loads(secret)
+    return json.loads(secret)
+
 
 def create_jira_issue(jira_client: JIRA, project_key: str, issue_type: str, event: dict) -> Issue:
-  """
-  Create a JIRA issue based on a Security Hub event.
+    """
+    Create a JIRA issue based on a Security Hub event.
 
-  Args:
-      jira_client (JIRA): An authenticated JIRA client instance.
-      project_key (str): The key of the JIRA project.
-      issue_type (str): The type of the JIRA issue.
-      event (Dict): The Security Hub event data.
+    Args:
+        jira_client (JIRA): An authenticated JIRA client instance.
+        project_key (str): The key of the JIRA project.
+        issue_type (str): The type of the JIRA issue.
+        event (Dict): The Security Hub event data.
 
-  Returns:
-      Issue: The created JIRA issue.
+    Returns:
+        Issue: The created JIRA issue.
 
-  Raises:
-      Exception: If there is an error creating the JIRA issue.
-  """
+    Raises:
+        Exception: If there is an error creating the JIRA issue.
+    """
 
-  finding = event['findings'][0]
-  finding_account_id = finding['AwsAccountId']
-  finding_title = finding['Title']
+    finding = event['findings'][0]
+    finding_account_id = finding['AwsAccountId']
+    finding_title = finding['Title']
 
-  issue_title = f"Security Hub ({finding_title}) detected in {finding_account_id}"
+    issue_title = f"Security Hub ({finding_title}) detected in {
+        finding_account_id}"
 
-  issue_description = f"""
-    {finding['Description']}
+    issue_description = f"""
+      {finding['Description']}
 
-    A Security Hub finding has been detected:
-    {{code}}{json.dumps(event, indent=2, sort_keys=True)}{{code}}
-  """
+      A Security Hub finding has been detected:
+      {{code}}{json.dumps(event, indent=2, sort_keys=True)}{{code}}
+    """
 
-  issue_labels = [
-    finding["Region"],
-    finding_account_id,
-    finding['Severity']['Label'].lower(),
-    *[finding['ProductFields'][key].replace(" ", "") for key in ["RuleId", "ControlId", "aws/securityhub/ProductName"] if key in finding['ProductFields']]
-  ]
+    issue_labels = [
+        finding["Region"],
+        finding_account_id,
+        finding['Severity']['Label'].lower(),
+        *[finding['ProductFields'][key].replace(" ", "")
+          for key in ["RuleId", "ControlId", "aws/securityhub/ProductName"]
+          if key in finding['ProductFields']]
+    ]
 
-  issue_dict = {
-    'project': {'key': project_key},
-    'issuetype': {'name': issue_type},
-    'summary': issue_title,
-    'description': issue_description,
-    'labels': issue_labels,
-    'customfield_11101': {'value': 'Vulnerability Management'}
-  }
+    issue_dict = {
+        'project': {'key': project_key},
+        'issuetype': {'name': issue_type},
+        'summary': issue_title,
+        'description': issue_description,
+        'labels': issue_labels,
+        'customfield_11101': {'value': 'Vulnerability Management'}
+    }
 
-  try:
-    issue = jira_client.create_issue(fields=issue_dict)
-    logger.info(f"Created JIRA issue: {issue.key}")
-    return issue
-  except Exception as e:
-    logger.error(f"Failed to create JIRA issue: {e}")
-    raise e
+    try:
+        issue = jira_client.create_issue(fields=issue_dict)
+        logger.info(f"Created JIRA issue: {issue.key}")
+        return issue
+    except Exception as e:
+        logger.error(f"Failed to create JIRA issue: {e}")
+        raise e
+
 
 def close_jira_issue(jira_client: JIRA, issue: Issue, transition_name: str, comment: str) -> None:
-  """
-  Close a JIRA issue.
+    """
+    Close a JIRA issue.
 
-  Args:
-      jira_client (JIRA): An authenticated JIRA client instance.
-      issue (Issue): The JIRA issue to close.
+    Args:
+        jira_client (JIRA): An authenticated JIRA client instance.
+        issue (Issue): The JIRA issue to close.
 
-  Raises:
-      Exception: If there is an error closing the JIRA issue.
-  """
+    Raises:
+        Exception: If there is an error closing the JIRA issue.
+    """
 
-  try:
-    jira_client.add_comment(issue, comment)
-    jira_client.transition_issue(issue, transition_name, comment=comment)
-    logger.info(f"Closed JIRA issue: {issue.key}")
-  except Exception as e:
-    logger.error(f"Failed to close JIRA issue: {e}")
-    raise e
+    try:
+        jira_client.add_comment(issue, comment)
+        jira_client.transition_issue(issue, transition_name, comment=comment)
+        logger.info(f"Closed JIRA issue: {issue.key}")
+    except Exception as e:
+        logger.error(f"Failed to close JIRA issue: {e}")
+        raise e
 
-def update_security_hub(client: BaseClient, finding_id: str, product_arn: str, status: str, note: str = "") -> None:
-  """
-  Update a Security Hub finding with the given status and note.
 
-  Args:
-      client (BaseClient): A boto3 client instance for Security Hub.
-      finding_id (str): The ID of the finding to update.
-      product_arn (str): The ARN of the product associated with the finding.
-      status (str): The new status for the finding.
-      note (str): A note to add to the finding.
+def update_security_hub(client: BaseClient, finding_id: str,
+                        product_arn: str, status: str, note: str = "") -> None:
+    """
+    Update a Security Hub finding with the given status and note.
 
-  Raises:
-      ValueError: If the client is not an instance of Security Hub.
-      ClientError: If there is an error updating the finding.
-  """
+    Args:
+        client (BaseClient): A boto3 client instance for Security Hub.
+        finding_id (str): The ID of the finding to update.
+        product_arn (str): The ARN of the product associated with the finding.
+        status (str): The new status for the finding.
+        note (str): A note to add to the finding.
 
-  # Validate that the client is an instance of botocore.client.SecurityHub
-  if client.meta.service_model.service_name != 'securityhub':
-    raise ValueError(f"Client must be an instance of botocore.client.SecurityHub. Got {type(client)} instead.")
+    Raises:
+        ValueError: If the client is not an instance of Security Hub.
+        ClientError: If there is an error updating the finding.
+    """
 
-  try:
-      kwargs = {}
-      if note:
-        kwargs['Note'] = {
-            'Text': note,
-            'UpdatedBy': 'securityhub-findings-manager-jira'
-        }
-      response = client.batch_update_findings(
-          FindingIdentifiers=[
-              {
-                  'Id': finding_id,
-                  'ProductArn': product_arn
-              }
-          ],
-          Workflow={'Status': status},
-          **kwargs
-      )
+    # Validate that the client is an instance of botocore.client.SecurityHub
+    if client.meta.service_model.service_name != 'securityhub':
+        raise ValueError(f"Client must be an instance of botocore.client.SecurityHub. Got {
+                         type(client)} instead.")
 
-      if response.get('FailedFindings'):
-        for element in response['FailedFindings']:
-          logger.error(f"Updating SecurityHub finding failed: FindingId {element['Id']}, ErrorCode {element['ErrorCode']}, ErrorMessage {element['ErrorMessage']}")
-      else:
-        logger.info(f"Updated SecurityHub finding {finding_id} to status {status}.")
+    try:
+        kwargs = {}
+        if note:
+            kwargs['Note'] = {
+                'Text': note,
+                'UpdatedBy': 'securityhub-findings-manager-jira'
+            }
+        response = client.batch_update_findings(
+            FindingIdentifiers=[
+                {
+                    'Id': finding_id,
+                    'ProductArn': product_arn
+                }
+            ],
+            Workflow={'Status': status},
+            **kwargs
+        )
 
-  except Exception as e:
-      logger.exception(f"Updating SecurityHub finding failed: {e}")
-      raise
+        if response.get('FailedFindings'):
+            for element in response['FailedFindings']:
+                logger.error(f"Updating SecurityHub finding failed: FindingId {
+                    element['Id']}, ErrorCode {element['ErrorCode']}, ErrorMessage {
+                        element['ErrorMessage']}")
+        else:
+            logger.info(f"Updated SecurityHub finding {
+                        finding_id} to status {status}.")
+
+    except Exception as e:
+        logger.exception(f"Updating SecurityHub finding failed: {e}")
+        raise e
