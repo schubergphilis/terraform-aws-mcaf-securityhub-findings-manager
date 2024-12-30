4775391f38afb0edd69cb9ae46adb2831ca9e30d
Marcel de Vroed <mdevroed@schubergphilis.com>
2024-12-30 17:17:21 +0100

    this are errors not warnings

--- files/lambda-artifacts/securityhub-findings-manager/strategize_findings_manager.py
+++ files/lambda-artifacts/securityhub-findings-manager/strategize_findings_manager.py
@@ -18,14 +18,14 @@ def manage(func, args, logger: Logger):
     try:
         findings_manager = _initialize_findings_manager(logger)
     except Exception as e:
-        logger.warning("Findings manager failed to initialize, please investigate.")
+        logger.error("Findings manager failed to initialize, please investigate.")
         logger.info(f"Original error: {e}", exc_info=True)
         return {"finding_state": "skipped"}
 
     try:
         success, suppressed_payload = getattr(findings_manager, func.__name__)(*args)
     except Exception as e:
-        logger.warning("Findings manager failed to apply findings management rules, please investigate.")
+        logger.error("Findings manager failed to apply findings management rules, please investigate.")
         logger.info(f"Original error: {e}", exc_info=True)
         return {"finding_state": "skipped"}
 
@@ -43,7 +43,7 @@ def manage(func, args, logger: Logger):
             logger.info("No findings were suppressed.")
             return {"finding_state": "skipped"}
     else:
-        logger.warning(
+        logger.error(
             "No explicit error was raised, but not all findings management rules were applied successfully, please investigate."
         )
         return {"finding_state": "skipped"}
